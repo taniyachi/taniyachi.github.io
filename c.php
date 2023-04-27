7e00f63324507a48b12ba6cac21271c3b99d8748
taniyachi <131984669+taniyachi@users.noreply.github.com>
2023-04-27 21:43:35 +0300

    Add files via upload

---
+++ c.php
@@ -0,0 +1,190 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+            
+		<title>Imam Guide location</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="main.css" />
+                <link href="https://fonts.googleapis.com/css2?family=Amiri&display=swap" rel="stylesheet">
+                <link rel="icon" type="image/x-icon" href="logo.png">
+             
+	</head>
+	<body class="landing is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="alt">
+					<h1><a href="c.php">دليل مكاتب </a>  جامعة الامام محمد بن سعود الاسلامية</h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="c.php">الصفحة الرئيسية</a></li>
+							<li>
+								<a href="#" class="icon solid fa-angle-down">الخرائط</a>
+								<ul>
+									<li><a href="map1.php">خريطة مكاتب الدور الاول</a></li>
+									<li><a href="map2.php">خريطة مكاتب الدورالثاني</a></li>
+									<li><a href="map3.php">خريطة مكاتب الدورالثالث</a></li>
+                                    <li><a href="map4.php">خريطة مكاتب الدورالرابع</a></li>
+								
+								</ul>
+							</li>
+							<li><a href="login.php" class="button">تسجيل الدخول</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>دليل</h2>
+					<p>مكاتب جامعة الامام محمد بن سعود الاسلامية</p>
+					<ul class="actions special">
+						
+						<!---<li><a href="#" class="button">المزيد عنا </a></li>---->
+					</ul>
+                    
+					<form method="post">
+						<ul  class="actions special">
+                   <input type="submit" name="submit" >
+<input style=" margin: center; width: 40%; padding:4px; position: center; color: black; " type="text" name="search" placeholder="اسم الاستاذة- اسم المكتب"></li>
+		 
+                                      </form>  </ul>  
+				</section>
+				<section>
+				<?php
+
+$con = new PDO("mysql:host=localhost;dbname=my",'root','');
+if (isset($_POST["submit"])) {
+    $str = $_POST["search"];
+	$sth = $con->prepare("SELECT * FROM `office` WHERE Name = '$str'");
+
+    $sth->setFetchMode(PDO::FETCH_OBJ);
+    $sth-> execute();
+    if ($row = $sth->fetch())
+    {
+
+   
+ 
+        
+        echo "<br/>";
+        echo "<table class='table table-hover'>";
+
+        echo "<thread>
+            <tr>
+                <th scope='col'> الاسم </th>
+                <th scope='col'>رقم المكتب</th>
+                <th scope='col'>الدور</th>
+                <th scope='col'>الدوار</th>
+                <th scope='col'>رقم الصيانة</th>
+				<th scope='col'>القسم</th>
+		</tr>
+        </thread>";
+		echo "<tr>";
+		             echo "<td>".$row-> name;"</td>";
+					 echo "<td>".$row->offNum."</td>";
+					 echo "<td>".$row->	floor; "</td>";
+					 echo "<td>". $row->round; "</td>";
+                     echo  "<td>". $row ->maintNum;"</td>";
+				     echo"<td>". $row-> dep;"</td>";
+					  
+					  
+					 echo "</tr>";
+
+			echo "</table>";
+         
+    }
+ 	
+   else{
+	echo "<h3>"."الاسم غير موجود "."</h3>";
+}}
+?>
+</section>
+ 
+
+    <h2 style="text-align: center;">جميع المكاتب</h2>
+	<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "my";
+
+// Create connection
+$conn = new mysqli("localhost",'root','','my');
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM office";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+ 
+   echo "<br/>";
+        echo "<table class='table table-hover'>";
+
+        echo "<thread>
+            <tr>
+                <th scope='col'> الاسم </th>
+                <th scope='col'>رقم المكتب</th>
+                <th scope='col'>الدور</th>
+                <th scope='col'>الدوار</th>
+                <th scope='col'>رقم الصيانة</th>
+				<th scope='col'>القسم</th>
+		</tr>
+        </thread>";
+		
+   foreach ($result as $row) {
+	
+            echo "<tr>";
+            echo "<td>" . $row["name"] . "</td>";
+            echo "<td>" . $row["offNum"] . "</td>";
+            echo "<td>" . $row["floor"] . "</td>";
+            echo "<td>" . $row["round"] . "</td>";
+            echo "<td>" . $row["maintNum"] . "</td>";
+			echo "<td>" . $row["dep"] . "</td>";
+ }
+			echo "</table>";
+        
+  
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>"
+	
+	
+	
+   
+		
+					<div class="row">
+						<div class="col-6 col-12-narrower">
+
+							<!--<section class="box special">
+								<span class="image featured"><img src="images/pic02.jpg" alt="" /></span>
+								<h3>#</h3>
+								<p> </p>
+								<ul class="actions special">
+									 <li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>--->
+
+						</div>
+					</div>
+
+				</section>
+
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
